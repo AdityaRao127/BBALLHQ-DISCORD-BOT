09c154aeb7076d0cb42b9ddfc3df7951aafdd208
Aditya Rao <rao.aditya.codes@gmail.com>
2024-04-27 20:44:57 -0700

    additions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Final
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv #test
 from discord import Intents, Client, Message
 from responses import get_response
 
