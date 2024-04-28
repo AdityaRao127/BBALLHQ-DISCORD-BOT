5d5b5f76ba16cc0afad6e57686082d06039da504
Aditya Rao <rao.aditya.codes@gmail.com>
2024-04-27 22:10:50 -0700

    working

--- responses.py
+++ responses.py
@@ -1,19 +1,20 @@
 from random import choice, randint
 
-
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
 
     if lowered == '':
         return 'Well, you\'re awfully silent...'
     elif 'hello' in lowered:
-        return 'Hello there!'
+        return 'Hello, what would you like to know?'
     elif 'how are you' in lowered:
         return 'Good, thanks!'
     elif 'bye' in lowered:
         return 'See you!'
     elif 'roll dice' in lowered:
         return f'You rolled: {randint(1, 6)}'
+    elif 'nba' in lowered:
+        return 'Choose an option: [NBA Standings, NBA News, Player Stats]'
     else:
         return choice(['I do not understand...',
                        'What are you talking about?',
